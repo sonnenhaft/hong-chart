28136c7610967137e178b2cd6eb4affa1951d25a
Vladimir Shein <vlad@cog1.com>
2016-05-01 19:34:35 +0200

    added dynamic colors

--- src/hong-tooltip/hong-tooltip.js
+++ src/hong-tooltip/hong-tooltip.js
@@ -10,28 +10,50 @@ angular.module('hc.hong-tooltip', [
         templateUrl: 'src/hong-tooltip/hong-tooltip.html',
         scope: { abatement: '=', charts: '=', year: '=' },
         link: function ( $scope, $element ) {
-
             var x = d3.scale.linear();
             var y = d3.scale.linear();
             var currentSvgElement = d3.select($element[ 0 ]).select('svg');
             var svg = currentSvgElement.select('.main');
 
-            function updateWidth() {
-                var htmlWidth = 150; //currentSvgElement[0][0].parentNode.offsetWidth;
-                var height = 35;
-                svg.attr(translate(margin.left, margin.top));
-                var width = htmlWidth - margin.left - margin.right;
-                x.range([ 0, width ]);
-                y.range([ height, 0 ]);
-                currentSvgElement.attr({
-                    width: width + margin.left + margin.right,
-                    height: height + margin.top + margin.bottom
+            var htmlWidth = 150; //currentSvgElement[0][0].parentNode.offsetWidth;
+            var height = 35;
+            svg.attr(translate(margin.left, margin.top));
+            var width = htmlWidth - margin.left - margin.right;
+            x.range([ 0, width ]);
+            y.range([ height, 0 ]);
+            currentSvgElement.attr({
+                width: width + margin.left + margin.right,
+                height: height + margin.top + margin.bottom
+            });
+
+            function render( data, opt_noTransition ) {
+                if ( !data || !data.length ) {return;}
+                var val = function ( d ) {return d.value;};
+                var maxYValue = d3.max(data, val);
+
+                x.domain([ 0, data.length ]);
+                y.domain([ maxYValue, 0 ]);
+
+                var height = function ( d ) { return y(d.value);};
+                var gap = 0.03;
+                var yCoord = function ( d ) {return y(maxYValue - d.value);};
+                var xCoord = function ( d, index ) {return x(index);};
+                svg.transition().duration(opt_noTransition ? 0 : 2000).each(function () {
+                    svg.select('.rect').attr(translate(x(gap), 0)).bindData('rect', data, {
+                        fill: function ( d ) {return d.color;},
+                        opacity: 0.8
+                    }, 'text').attr({ x: xCoord, width: x(1) * (1 - gap * 2) })
+                        .transition().attr({ height: height, y: yCoord });
+
+                    svg.select('.text').attr(translate(x(0.5), 0)).bindData('text', data, {}, 'text')
+                        .text(function ( d ) { return d.value; }).attr({ x: xCoord })
+                        .transition().attr({ y: yCoord });
                 });
             }
 
+            var data;
             $scope.$watchGroup([ 'charts.$version', 'year' ], function () {
                 if ( !$scope.abatement ) {return;}
-                var maxLength = $scope.charts[ 0 ].years.length;
                 data = $scope.abatement.filter(function ( selection ) {
                     return selection.$selected;
                 }).map(function ( selection ) {
@@ -41,44 +63,10 @@ angular.module('hc.hong-tooltip', [
                         color: selection.color,
                         text: selection.name,
                         value: d3.sum(sel.years.slice(0, $scope.year - shift))
-                    }
+                    };
                 });
-                $timeout(function () {render(data);})
+                $timeout(function () {render(data);});
             });
-
-            function render( data, opt_noTransition ) {
-                if ( !data || !data.length ) {return;}
-                var val = function ( d ) {return d.value};
-                var maxYValue = d3.max(data, val);
-
-                x.domain([ 0, data.length ]);
-                y.domain([ maxYValue, 0 ]);
-
-                var height = function ( d ) { return y(d.value);};
-                var gap = 0.03;
-                var yCoord = function ( d ) {return y(maxYValue - d.value)};
-                var xCoord = function ( d, index ) {return x(index);};
-                svg.transition().duration(opt_noTransition ? 0 : 2000).each(function () {
-                    svg.select('.rect').attr(translate(x(gap), 0)).bindData('rect', data, {
-                        fill: function(d){return d.color;},
-                        opacity: 0.8
-                    }, 'text').attr({
-                        x: xCoord,
-                        width: x(1) * (1 - gap * 2)
-                    }).transition().attr({
-                        height: height,
-                        y: yCoord
-                    });
-
-                    svg.select('.text').attr(translate(x(0.5), 0)).bindData('text', data, {}, 'text').text(function ( d ) {
-                        return d.value;
-                    }).attr({x: xCoord}).transition().attr({ y: yCoord });
-                });
-            }
-
-            var data;
-
-            updateWidth();
         }
     };
 });
--- src/svg-hong-chart/svg-hong-chart.directive.js
+++ src/svg-hong-chart/svg-hong-chart.directive.js
@@ -2,48 +2,48 @@ angular.module('hc.svg-hong-chart', [
     'hc.data-utilites',
     'hc.d3.bind-data'
 ]).directive('svgHongChart', function ( DataUtilites, $window, d3 ) {
-    var margin = {top: 15, right: 20, bottom: 33, left: 50};
+    var margin = { top: 15, right: 20, bottom: 33, left: 50 };
 
-    function translate( x, y ) {return {transform: 'translate(' + x + ',' + y + ')'};}
+    function translate( x, y ) {return { transform: 'translate(' + x + ',' + y + ')' };}
 
-    function key( k ) {return function ( d ) {return d[k];}; }
+    function key( k ) {return function ( d ) {return d[ k ];}; }
 
     return {
         restrict: 'A',
         templateUrl: 'src/svg-hong-chart/svg-hong-chart.html',
-        scope: {data: '=svgHongChart', tooltip: '=', tooltipYear: '='},
+        scope: { data: '=svgHongChart', tooltip: '=', tooltipYear: '=' },
         link: function ( $scope, $element ) {
             var x = d3.scale.linear();
             var y = d3.scale.linear();
             var xAxis = d3.svg.axis().orient('bottom').tickSize(4, 0);
             var yAxis = d3.svg.axis().orient('left').tickSize(4, 0);
 
-            var currentSvgElement = d3.select($element[0]).select('svg');
+            var currentSvgElement = d3.select($element[ 0 ]).select('svg');
             var svg = currentSvgElement.select('.main');
 
             var scaleFactor;
 
             $scope.tooltipFn = function ( yearIndex, chartIndex, toShow ) {
                 $scope.tooltipYear = yearIndex;
-                var indexes = [chartIndex];
+                var indexes = [ chartIndex ];
                 if ( chartIndex === 0 || chartIndex === $scope.data.length - 1 ) {
-                    indexes = [0, $scope.data.length - 1];
+                    indexes = [ 0, $scope.data.length - 1 ];
                 }
                 indexes.forEach(function ( chartIndex ) {
-                    $scope.data[chartIndex].$highlight = toShow;
+                    $scope.data[ chartIndex ].$highlight = toShow;
                 });
 
                 $scope.$apply();
             };
 
             function updateWidth() {
-                var htmlWidth = currentSvgElement[0][0].parentNode.offsetWidth;
+                var htmlWidth = currentSvgElement[ 0 ][ 0 ].parentNode.offsetWidth;
                 scaleFactor = htmlWidth > 400 ? 1 : (htmlWidth / 400);
                 svg.attr(translate(margin.left, margin.top));
                 var width = htmlWidth - margin.left - margin.right;
                 var height = htmlWidth / 2 - margin.top - margin.bottom;
-                x.range([0, width]);
-                y.range([height, 0]);
+                x.range([ 0, width ]);
+                y.range([ height, 0 ]);
                 xAxis.scale(x);
                 yAxis.scale(y);
                 svg.select('.x-line').attr('y1', height);
@@ -53,7 +53,7 @@ angular.module('hc.svg-hong-chart', [
                     width: width + margin.left + margin.right,
                     height: height + margin.top + margin.bottom
                 });
-                svg.attr({'font-size': 130 * scaleFactor + '%'});
+                svg.attr({ 'font-size': 130 * scaleFactor + '%' });
                 svg.selectAll('.x.axis, .logo').attr(translate(0, height));
                 svg.select('.x-axis-label').attr(translate(width / 2, height));
                 svg.select('.y-axis-label').attr(translate(0, height / 2));
@@ -67,11 +67,11 @@ angular.module('hc.svg-hong-chart', [
                     return index === 0 || index === data.length - 1 || d.$selected;
                 });
                 var yRange = DataUtilites.getYRange(filteredData);
-                var firstChart = data[0];
-                x.domain([0, firstChart.years.length - 1]);
+                var firstChart = data[ 0 ];
+                x.domain([ 0, firstChart.years.length - 1 ]);
                 /** uncomment if will decided to start axis from min value instead of ziro */
                     // y.domain([ yRange.min  / 1.01, yRange.max * 1.01 ]);
-                y.domain([0, yRange.max * 1.01]);
+                y.domain([ 0, yRange.max * 1.01 ]);
 
                 svg.select('.x.axis').call(xAxis.tickFormat(function ( d ) {return opt_offsetArg + d;}));
                 svg.select('.y.axis').call(yAxis);
@@ -84,11 +84,11 @@ angular.module('hc.svg-hong-chart', [
                     var definedIndex = years.lastIndexOf(undefined);
                     return years.map(function ( y, x ) {
                         if ( y === undefined ) {
-                            return {x: definedIndex, y: firstChart.years[definedIndex]};
+                            return { x: definedIndex, y: firstChart.years[ definedIndex ] };
                         } else {
-                            return {x: x, y: y};
+                            return { x: x, y: y };
                         }
-                    })
+                    });
                 }
 
                 var tooltip = $scope.tooltip;
@@ -99,27 +99,27 @@ angular.module('hc.svg-hong-chart', [
                             return d.style === 'dashed' ? '10,3' : 0;
                         },
                         'stroke-width': key('width')
-                    }, 'id').transition().attr({d: function ( chart ) {return line(cover(chart.years));}});
+                    }, 'id').transition().attr({ d: function ( chart ) {return line(cover(chart.years));} });
 
                     svg.select('.chart-lines').bindData('g', filteredData, {
                         fill: key('color'),
                         'stroke-width': 1
                     }, 'id').bindData('circle', function ( data ) {
                         return cover(data.years);
-                    }).attr({r: scaleFactor * 4}).transition().attr({cx: xCoord, cy: yCoord});
+                    }).attr({ r: scaleFactor * 4 }).transition().attr({ cx: xCoord, cy: yCoord });
 
                     svg.select('.chart-lines-hover').bindData('g', filteredData, null, 'id').bindData('circle', function ( data ) {
                         return cover(data.years);
                     }).on({
                         mouseenter: function ( d, yearIndex, currentChartIndex ) {
                             tooltipFn(yearIndex, currentChartIndex, true);
-                            var circles = svg.selectAll('.chart-lines g circle:nth-child(' + (yearIndex + 1) + ')').attr({r: scaleFactor * 6});
-                            var circle = d3.select(circles[0][currentChartIndex]).classed({big: true});
+                            var circles = svg.selectAll('.chart-lines g circle:nth-child(' + (yearIndex + 1) + ')').attr({ r: scaleFactor * 6 });
+                            var circle = d3.select(circles[ 0 ][ currentChartIndex ]).classed({ big: true });
                             var x = circle.attr('cx');
                             var y = circle.attr('cy');
-                            var targetLines = svg.select('.target-lines').attr({opacity: 0.2});
-                            targetLines.select('.x-line').attr({x1: x, x2: x});
-                            targetLines.select('.y-line').attr({y1: y, y2: y});
+                            var targetLines = svg.select('.target-lines').attr({ opacity: 0.2 });
+                            targetLines.select('.x-line').attr({ x1: x, x2: x });
+                            targetLines.select('.y-line').attr({ y1: y, y2: y });
 
                             tooltip.style({
                                 opacity: 0.9,
@@ -129,22 +129,22 @@ angular.module('hc.svg-hong-chart', [
                         },
                         mouseleave: function ( d, yearIndex, chartIndex ) {
                             tooltipFn(yearIndex, chartIndex, false);
-                            svg.selectAll('.chart-lines g')[0].forEach(function ( d ) {
-                                d3.select(d3.select(d).selectAll('circle')[0][yearIndex]).attr({r: scaleFactor * 4}).classed({
+                            svg.selectAll('.chart-lines g')[ 0 ].forEach(function ( d ) {
+                                d3.select(d3.select(d).selectAll('circle')[ 0 ][ yearIndex ]).attr({ r: scaleFactor * 4 }).classed({
                                     big: false
                                 });
                             });
-                            svg.select('.target-lines').attr({opacity: 0});
+                            svg.select('.target-lines').attr({ opacity: 0 });
                             tooltip.style('opacity', 0);
                         }
-                    }).attr({cx: xCoord, cy: yCoord, r: scaleFactor * 12});
+                    }).attr({ cx: xCoord, cy: yCoord, r: scaleFactor * 12 });
 
-                    var lastChart = filteredData[filteredData.length - 1];
-                    svg.select('.bau-reduce-area').attr({visibility: 'visible'}).transition().attr({
+                    var lastChart = filteredData[ filteredData.length - 1 ];
+                    svg.select('.bau-reduce-area').attr({ visibility: 'visible' }).transition().attr({
                         d: d3.svg.area().y0(y).x(function ( d, i ) {
                             return x(i);
                         }).y1(function ( d, i ) {
-                            return y(lastChart.years[i]);
+                            return y(lastChart.years[ i ]);
                         })(firstChart.years)
                     });
                 });
@@ -152,7 +152,7 @@ angular.module('hc.svg-hong-chart', [
 
             updateWidth();
 
-            $scope.$watchGroup(['data', 'data.$version'], function () {
+            $scope.$watchGroup([ 'data', 'data.$version' ], function () {
                 render($scope.data || [], 2016);
             });
 
